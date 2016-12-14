846f76368c0dbc25caf1b3ca94ee12ba472c4a2c
svcarlsen <svc@kb.dk>
2016-12-14 18:43:04 +0100

    Removing libs from automatic-workflow/lib - refactoring webdanica-analysis-cron so webdanica-analysis-manual.sh could be added easily

--- automatic-workflow/webdanica-analysis-cron.sh
+++ automatic-workflow/webdanica-analysis-cron.sh
@@ -1,16 +1,11 @@
-## possibly put this in bash_profile
-export WORKFLOW_USER_HOME=/home/test
-export WORKFLOW_HOME=$WORKFLOW_USER_HOME/automatic-workflow
-export WEBDATADIR=$WORKFLOW_USER_HOME/ARKIV
-export WEBDANICA_VERSION=0.4.0-SNAPSHOT
-export HADOOP_HOME=$WORKFLOW_USER_HOME/hadoop-1.2.1/
-export PIG_HOME=$WORKFLOW_USER_HOME/pig-0.16.0/
-export BUSYFILE=$WORKFLOW_HOME/.busy
-export WORKDIR=$WORKFLOW_HOME/working
-export OLDJOBSDIR=$WORKFLOW_HOME/oldjobs
-export JAVA_HOME=/usr/java/jdk1.8.0_92_x64
-export PATH=$JAVA_HOME/bin:$PATH
-#echo PATH used: $PATH
+SETENV=/home/test/automatic-workflow/setenv.sh
+ME=`basename $0`
+if [ -r "$SETENV" ]; then
+  . "$SETENV"
+else 
+  echo Error: Path to setenv.sh is not correctly set in script $ME
+  exit 1
+fi
 
 if [ -f $BUSYFILE ]; then
    STAT=`stat -c %y $BUSYFILE` 
@@ -20,17 +15,14 @@ fi
 ## mark the workflow as being in progress
 touch $BUSYFILE
 
-FINDLOGS_SCRIPT=${WORKFLOW_HOME}/findharvestlogs.sh
-AUTOMATIC_SCRIPT=${WORKFLOW_HOME}/automatic.sh
-
 if [ ! -f $FINDLOGS_SCRIPT ]; then
-   echo "ERROR: The script '$FINDLOGS_SCRIPT' does not exist. Exiting program"
+   echo "ERROR: The script '$FINDLOGS_SCRIPT' does not exist. Exiting program $ME"
    rm $BUSYFILE	
    exit 1
  fi
 
 if [ ! -f $AUTOMATIC_SCRIPT ]; then
-   echo "ERROR: The script '$AUTOMATIC_SCRIPT' does not exist. Exiting program"
+   echo "ERROR: The script '$AUTOMATIC_SCRIPT' does not exist. Exiting program $ME"
    rm $BUSYFILE
    exit 1
  fi
@@ -42,7 +34,7 @@ cd $WORKFLOW_HOME
 FILES=`bash $FINDLOGS_SCRIPT $WORKFLOW_HOME $WEBDANICA_VERSION`
 RESCODE=$?
 if [ -z $RESCODE ]; then
-   echo "ERROR: The script '$FINDLOGS_SCRIPT' failed. Exiting program"
+   echo "ERROR: The script '$FINDLOGS_SCRIPT' failed. Exiting program $ME"
    rm $BUSYFILE
    exit 1	
 fi
@@ -64,7 +56,7 @@ HARVESTLOG=$WORKDIR/$NAME
 mv $J $HARVESTLOG
 RESCODE=$?
 if [ -z $RESCODE ]; then
-   echo "ERROR: Failed to move the file $J to $HARVESTLOG. Exiting program"
+   echo "ERROR: Failed to move the file $J to $HARVESTLOG. Exiting program $ME"
    rm $BUSYFILE
    exit 1
 fi
@@ -73,7 +65,7 @@ fi
 bash $AUTOMATIC_SCRIPT $HARVESTLOG $WORKFLOW_HOME $WEBDATADIR $WEBDANICA_VERSION $HADOOP_HOME $PIG_HOME
 RESCODE=$?
 if [ -z $RESCODE ]; then
-   echo "ERROR: The $AUTOMATIC_SCRIPT returned $RESCODE. Exiting program"
+   echo "ERROR: The $AUTOMATIC_SCRIPT returned $RESCODE. Exiting program $ME"
 fi
 
 ## move $HARVESTLOG to OLDJOBSDIR
