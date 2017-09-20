7a4aca0884bad1c3b24be30d5f1aa27c821cf620
Nicholas Clarke <nicl@kb.dk>
2017-09-20 13:24:18 +0200

    Added and fixed drop in hbase-phoenix ddl.

--- webdanica-core/src/main/resources/scripts/hbase-phoenix/create_domains.sql
+++ webdanica-core/src/main/resources/scripts/hbase-phoenix/create_domains.sql
@@ -1,21 +1,21 @@
 DROP INDEX IF EXISTS domains_updated_time_idx ON domains;
 DROP INDEX IF EXISTS domains_danicastatus_idx ON domains;
+DROP INDEX IF EXISTS domains_tld_idx ON domains;
 DROP TABLE IF EXISTS domains;
 
 CREATE TABLE domains (
-    domain VARCHAR PRIMARY KEY,
-    danicastatus INTEGER,
-    inserted_time TIMESTAMP,
-    updated_time TIMESTAMP,
-    danicastatus_reason VARCHAR(256),
-    tld VARCHAR(64),
-    danica_parts VARCHAR[],
-    analyzed INTEGER,
-    danicacount INTEGER,
-    notes VARCHAR
+  domain VARCHAR PRIMARY KEY,
+  danicastatus INTEGER,
+  inserted_time TIMESTAMP,
+  updated_time TIMESTAMP,
+  danicastatus_reason VARCHAR(256),
+  tld VARCHAR(64),
+  danica_parts VARCHAR[],
+  analyzed INTEGER,
+  danicacount INTEGER,
+  notes VARCHAR
 );
 
 CREATE INDEX domains_updated_time_idx ON domains (updated_time);
-CREATE INDEX domains_danica_idx ON domains (danicastatus);
+CREATE INDEX domains_danicastatus_idx ON domains (danicastatus);
 CREATE INDEX domains_tld_idx ON domains (tld);
-
--- webdanica-core/src/main/resources/scripts/hbase-phoenix/create_harvests.sql
+++ webdanica-core/src/main/resources/scripts/hbase-phoenix/create_harvests.sql
@@ -4,17 +4,17 @@ DROP INDEX IF EXISTS harvest_successful_idx ON harvests;
 DROP TABLE IF EXISTS harvests;
 
 CREATE TABLE harvests (
-    harvestname VARCHAR PRIMARY KEY,
-    seedurl VARCHAR,
-    error VARCHAR,
-    successful BOOLEAN,
-    finalState INTEGER,
-    harvested_time BIGINT,
-    files VARCHAR[],
-    fetched_urls VARCHAR[],
-    analysis_state INTEGER,
-    analysis_state_reason VARCHAR,
-    reports VARCHAR[]
+  harvestname VARCHAR PRIMARY KEY,
+  seedurl VARCHAR,
+  error VARCHAR,
+  successful BOOLEAN,
+  finalState INTEGER,
+  harvested_time BIGINT,
+  files VARCHAR[],
+  fetched_urls VARCHAR[],
+  analysis_state INTEGER,
+  analysis_state_reason VARCHAR,
+  reports VARCHAR[]
 );
 
 CREATE INDEX harvest_finalstate_idx ON harvests (finalState);
