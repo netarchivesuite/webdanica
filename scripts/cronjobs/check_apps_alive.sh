e385fa9ea5d5aad0d0cff495404d0e6ef0dbb97f
Søren Vejrup Carlsen <svc@kb.dk>
2017-10-11 18:35:31 +0200

    Fixed WEBDAN-266 - now checks the number of NetarchiveSuite apps alive as well

--- scripts/cronjobs/check_apps_alive.sh
+++ scripts/cronjobs/check_apps_alive.sh
@@ -1,5 +1,9 @@
 ## checks if the two webdanica websites on port 8080 and 8074 are answering.
 ## argument: the HOST of the webdanica system, e.g. http://kb-test-webdanica-001.kb.dk
+## Also checks that the 3 WEBDANICA apps are running on the server
+NAS_APPS_COUNT_REQUIRED=3
+NAS_ENV=WEBDANICA
+
 ME=`basename $0`
 HOST=$1
 if [ -z "$HOST" ]; then
@@ -20,4 +24,9 @@ fi
 if [ $WEBDANICA_APP_CHECK -ne 0 ]; then
         echo "Page $WEBDANICA_APP_STATUS_PAGE was down at $TIME!" 
 fi
+## Finally, check the number of NAS WEBDANICA apps on the machine
+NAS_APPS_COUNT_FOUND=`ps auxwwww | grep $NAS_ENV | grep java | wc -l`
 
+if [ $NAS_APPS_COUNT_FOUND != $NAS_APPS_COUNT_REQUIRED ]; then
+        echo "Only found $NAS_APPS_COUNT_FOUND NetarchiveSuite apps, required is $NAS_APPS_COUNT_REQUIRED"
+fi
